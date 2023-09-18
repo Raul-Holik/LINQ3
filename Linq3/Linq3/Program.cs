10a26901854bc6a82fa6712cbe48e754539848bb
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2023-09-18 11:13:24 +0200

    Add files via upload

---
+++ Linq3/Linq3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq3
+{
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] studentArray = {
+            new Student() { StudentID = 1, StudentName = "John", Age = 18 },
+            new Student() { StudentID = 2, StudentName = "Steve",  Age = 21 },
+            new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 },
+            new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 },
+            new Student() { StudentID = 5, StudentName = "Ron" , Age = 31 },
+            new Student() { StudentID = 6, StudentName = "Chris",  Age = 17 },
+            new Student() { StudentID = 7, StudentName = "Rob",Age = 19  },
+        };
+
+            Student[] students = new Student[10];
+           
+            var studentSort= from s in studentArray
+                             where s.Age >12&& s.Age<20
+                             orderby s.Age descending
+                             select s;
+
+            foreach(Student student in studentSort)
+            {
+                Console.WriteLine(student.StudentName+" Age: "+ student.Age);
+            }
+            Console.ReadKey();
+        }
+    }
+}
